8d2083a9633c6942b8a78fb46ab39c1a7f8e4255
Michi Kono <michikono@gmail.com>
2015-07-11 12:32:13 -0700

    made shit look nice

--- client/app/read/read.controller.js
+++ client/app/read/read.controller.js
@@ -1,12 +1,20 @@
 'use strict';
 
 angular.module('blockchainedMelodyApp')
-  .controller('ReadCtrl', function ($scope, $http) {
+  .controller('ReadCtrl', function ($scope, $http, $stateParams) {
     $scope.text = '';
+    $scope.asset = {
+      definition_reference: {
+        name: $stateParams.assetTitle
+      }
+    };
 
-	$http.get('api/text').success(function(text) {
-		$scope.text = text[0].text;
-	});
+    $http.get('api/text').success(function (text) {
+      $scope.text = text[0].text;
+    });
 
+    $http.get('/api/assets/' + $stateParams.assetId).success(function (asset) {
+      $scope.asset = asset
+    });
 
   });
--- client/app/read/read.js
+++ client/app/read/read.js
@@ -3,8 +3,13 @@
 angular.module('blockchainedMelodyApp')
   .config(function ($stateProvider) {
     $stateProvider
-  	  .state('read', {
-        url: '/read',
+      .state('read', {
+        url: '/read/:assetId',
+        templateUrl: 'app/read/read.html',
+        controller: 'ReadCtrl'
+      })
+      .state('readWithTitle', {
+        url: '/read/:assetId/:assetTitle',
         templateUrl: 'app/read/read.html',
         controller: 'ReadCtrl'
       });
