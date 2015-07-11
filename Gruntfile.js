3cc03a75704e5de94d084a60122f0aa4b3e2bc3e
Dig V <dig@monsoonco.com>
2015-07-11 09:52:19 -0700

    pulls out karma in grunt calls

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function (grunt) {
           '<%= yeoman.client %>/{app,components}/**/*.spec.js',
           '<%= yeoman.client %>/{app,components}/**/*.mock.js'
         ],
-        tasks: ['newer:jshint:all', 'karma']
+        tasks: ['newer:jshint:all']
       },
       injectSass: {
         files: [
@@ -614,8 +614,7 @@ module.exports = function (grunt) {
         'injector:sass', 
         'concurrent:test',
         'injector',
-        'autoprefixer',
-        'karma'
+        'autoprefixer'
       ]);
     }
 
